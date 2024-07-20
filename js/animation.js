110260652cb9bb5b24417dad8d1b490d289a9723
ilan990 <77548546+ilan990@users.noreply.github.com>
2024-07-20 02:01:51 +0200

    Animation

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const mairieSection = document.querySelector("#mairie");
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          mairieSection.classList.add("animate__fadeInLeft");
-          observer.unobserve(mairieSection);
-        }
-      });
-    }, { threshold: 0.1 });
-
-    observer.observe(mairieSection);
+const handleIntersection = (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Récupérer l'animation spécifique de l'attribut data-animation
+        const animationClass = entry.target.getAttribute('data-animation');
+        // Ajouter la classe d'animation
+        entry.target.classList.add(animationClass);
+        // Optionnel : arrêtez d'observer l'élément une fois l'animation appliquée
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+  
+  // Créer un nouvel observateur
+  const observer = new IntersectionObserver(handleIntersection, { threshold: 0.1 });
+  
+  // Sélectionner tous les éléments avec une animation
+  const targets = document.querySelectorAll('[data-animation]');
+  targets.forEach(target => {
+    observer.observe(target);
   });
